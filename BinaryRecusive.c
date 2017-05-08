e2face5b006a5120959d0548c2ad559323c467c8
VishalAgarwal <agvish@users.noreply.github.com>
2017-05-08 20:11:09 +0530

    Binary Search(Recursive)

--- BinaryRecusive.c
+++ BinaryRecusive.c
@@ -1,22 +1,31 @@
+/*Program that will search for a key in a given set of input as an array. The searching algorithm used here is recursive binary search*/
+
+
 #include<stdio.h>
 #include<conio.h>
-int binRecurSrch(int*,int,int,int);
+int binRecurSrch(int*,int,int,int);         /*function prototype for the recursive binary search*/
 int main(void)
 {
+    /*Variable to store size, lower bound, upper bound of array, result of search. */
+    
     int size,l,h,k,i,result;
+    
+    /*A pointer variable to declare 1-d array dynamically*/
+  
+    int *arr;
     result=0;
     printf("\nEnter the size of array : ");
     scanf("%d",&size);
-    int *arr;
     arr=malloc(sizeof(int) * size);
     printf("\nEnter the %d elements of array in sorted order.",size);
     for(i=0;i<size;i++)
      scanf("%d",&arr[i]);
     printf("\nEnter the element to be searched.");
     scanf("%d",&k);
+    /*Assigning the value of lower bound and the upper bound*/
     l=0;
     h=size-1;
-    result=binRecurSrch(arr,l,h,k);
+    result=binRecurSrch(arr,l,h,k);               //call to recursive binary search function
     if(result==-1)
      printf("\nElement not found.");
     else
@@ -27,16 +36,18 @@ int main(void)
     getch();
     return 0;
 }
+
+/*Definition of binary search recursive function*/
 int binRecurSrch(int *a,int low,int high,int key)
 {
     int m;
-    m=(low+high)/2;
-    if(low>high)
+    m=(low+high)/2;  /*Calculating the middle position of array*/
+    if(low>high)    /*Base case*/
      return -1;
-    if(a[m]==key)
+    if(a[m]==key)    /*Key element found at middle position*/
      return m;
-    else if(a[m]>key)
-     return binRecurSrch(a,low,m-1,key);
-    else
-     return binRecurSrch(a,m+1,high,key);
+    else if(a[m]>key)    /*Key element value is less than that of the value at middle position*/
+     return binRecurSrch(a,low,m-1,key);           /*Decreasing the upper bound*/
+    else                /*Key element value is greater than that of the value at middle position*/
+     return binRecurSrch(a,m+1,high,key);           /*Increasing the lower bound*/
 }
