d5c1f0662615fd702655aa66ed60f77179ea0aa4
VishalAgarwal <vishalsunilagg@hotmail.com>
2017-07-08 17:04:36 +0530

    Update Binary Search Tree
    Problem in deleting nodes is now rectified.

--- BST.c
+++ BST.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
-struct node									/*Structure Definition for node*/
+struct node											/*Structure Definition for node*/
 {
 	int info;
 	struct node *left;
@@ -9,15 +9,15 @@ struct node									/*Structure Definition for node*/
 };
 typedef struct node *NODEPTR;
 
-void insert(NODEPTR*, int);								/*function prototype to insert a node in tree*/
-void deleteNode(NODEPTR*, int);							/*function prototype to delete a node from tree*/
-void preorder(NODEPTR);								/*function prototype to perform preorder traversal*/
-void postorder(NODEPTR);								/*function prototype to perform postorder traversal*/
-void inorder(NODEPTR);								/*function prototype to perform inorder traversal*/
-int countNode(NODEPTR);								/*function prototype to count total number of nodes in a tree*/
-int treeHeight(NODEPTR);								/*function prototype to calculate height of tree*/
+void insert(NODEPTR*, int);									/*function prototype to insert a node in tree*/
+void deleteNode(NODEPTR*, int);									/*function prototype to delete a node from tree*/
+void preorder(NODEPTR);										/*function prototype to perform preorder traversal*/
+void postorder(NODEPTR);									/*function prototype to perform postorder traversal*/
+void inorder(NODEPTR);										/*function prototype to perform inorder traversal*/
+int countNode(NODEPTR);										/*function prototype to count total number of nodes in a tree*/
+int treeHeight(NODEPTR);									/*function prototype to calculate height of tree*/
 int max(int, int);
-int search(NODEPTR, int);								/*function prototype to search for a particular node value*/
+int search(NODEPTR, int);									/*function prototype to search for a particular node value*/
 int main()
 {
 	struct node *bst;
@@ -41,22 +41,22 @@ int main()
 					break;
 			case 1: printf("\nEnter a NODE value : ");
 					scanf("%d",&x);
-					insert(&bst,x);				/*function call to insert a node into the BST*/
+					insert(&bst,x);						/*function call to insert a node into the BST*/
 					break;
 			case 2: printf("\nEnter a NODE value to be removed : ");
 					scanf("%d",&x);
-					deleteNode(&bst,x);				/*function call to remove a node from the BST*/
+					deleteNode(&bst,x);					/*function call to remove a node from the BST*/
 					break;
 			case 3: printf("\nPreorder Traversal results in : ");
-					preorder(bst);				/*function call to perform PREORDER traversal*/
+					preorder(bst);						/*function call to perform PREORDER traversal*/
 					break;
 			case 4: printf("\nPostorder Traversal results in : ");
-					postorder(bst);				/*function call to perform POSTORDER traversal*/
+					postorder(bst);						/*function call to perform POSTORDER traversal*/
 					break;
 			case 5: printf("\nInorder Traversal results in : ");
-					inorder(bst);				/*function call to perform INORDER traversal*/
+					inorder(bst);						/*function call to perform INORDER traversal*/
 					break;
-			case 6: count = countNode(bst);				/*function call to count total number of nodes in BST*/
+			case 6: count = countNode(bst);						/*function call to count total number of nodes in BST*/
 					printf("\nThere are %d nodes in the BST.",count);
 					break;
 			case 7: height = treeHeight(bst);					/*function call to calculate height of BST*/
@@ -64,7 +64,7 @@ int main()
 					break;
 			case 8: printf("\nEnter a NODE value to be searched : ");
 					scanf("%d",&x);
-					src = search(bst,x);				/*function call to search for a node value.*/
+					src = search(bst,x);					/*function call to search for a node value.*/
 					if(src == 0)
 						printf("\nUnsuccessful Search.");
 					else
@@ -76,12 +76,12 @@ int main()
 	getch();
 	return 0;
 }
-void insert(NODEPTR *proot,int val)							/*function definition to insert a node in the BST*/
+void insert(NODEPTR *proot,int val)								/*function definition to insert a node in the BST*/
 {
-	NODEPTR newNode, currNode;							/*variable to hold new and current node*/
+	NODEPTR newNode, currNode;								/*variable to hold new and current node*/
 	int found=0, flag = 0;
-	found = search(*proot,val);							/*function call top search a value*/
-	if(found == 1)								/*check for duplicate values*/
+	found = search(*proot,val);								/*function call top search a value*/
+	if(found == 1)										/*check for duplicate values*/
 	{
 		printf("\nNode with value %d is already present in BST.\nTry Again!.",val);
 		return;
@@ -90,32 +90,32 @@ void insert(NODEPTR *proot,int val)							/*function definition to insert a node
 	newNode->info = val;
 	newNode->left = NULL;
 	newNode->right = NULL;
-	if(*proot == NULL)								/*Check for empty BST*/
+	if(*proot == NULL)									/*Check for empty BST*/
 	{
 		*proot = newNode;
 	}
 	else
 	{
-		currNode = *proot;							/*Holds the root of the BST*/
+		currNode = *proot;								/*Holds the root of the BST*/
 		do
 		{
-			if(val < currNode->info)					/*Check for value less than value in current node*/
+			if(val < currNode->info)						/*Check for value less than value in current node*/
 			{
-				if(currNode->left != NULL)				/*move left of BST*/
+				if(currNode->left != NULL)					/*move left of BST*/
 					currNode = currNode->left;
 				else
 				{
-					currNode->left = newNode;			/*Insert as the left child of node*/
+					currNode->left = newNode;				/*Insert as the left child of node*/
 					flag = 1;
 				}
 			}
-			if(val > currNode->info)					/*check if value is greater than value in current node*/
+			if(val > currNode->info)						/*check if value is greater than value in current node*/
 			{
 				if(currNode->right != NULL)
-					currNode = currNode->right;			/*move right of BST*/
+					currNode = currNode->right;				/*move right of BST*/
 				else
 				{
-					currNode->right = newNode;			/*insert as right child of node*/
+					currNode->right = newNode;				/*insert as right child of node*/
 					flag = 1;
 				}
 			}
@@ -127,8 +127,8 @@ void deleteNode(NODEPTR *proot, int val)							/*function definition to delete a
 {
 	int found = 0, flag = 0;
 	NODEPTR delNode, currNode, parent, insucc, pinsucc, temp;
-	found = search(*proot,val);							/*function call to search a value*/
-	if(found == 0)								/*check whether the value to be deleted is present or not.*/
+	found = search(*proot,val);								/*function call to search a value*/
+	if(found == 0)										/*check whether the value to be deleted is present or not.*/
 	{
 		printf("\nNode with value %d not found.",val);
 		return;
@@ -138,65 +138,65 @@ void deleteNode(NODEPTR *proot, int val)							/*function definition to delete a
 		printf("\nEMPTY BST");
 		return;
 	}
-	currNode = *proot;								/*holds the current node*/
-	parent = NULL;								/*holds the parent node*/
+	currNode = *proot;									/*holds the current node*/
+	parent = NULL;										/*holds the parent node*/
 	insucc = pinsucc = NULL;
 	do
 	{
-		if(currNode->info == val)						/*check if the node value matches*/
+		if(currNode->info == val)							/*check if the node value matches*/
 		{
 			flag = 1;
 			delNode = currNode;
-			if(currNode->left == NULL && currNode->right == NULL)		/*check for leaf node*/
+			if(currNode->left == NULL && currNode->right == NULL)			/*check for leaf node*/
 			{
-				if(parent == NULL)								/*check if there is only one node in the tree*/
+				if(parent == NULL)						/*check if there is only one node in the tree*/
 				{
 					*proot = NULL;
 					break;
 				}
-				if(parent->right == currNode)				/*check whether the node was right child of its parent*/
+				if(parent->right == currNode)					/*check whether the node was right child of its parent*/
 					parent->right = NULL;									
-				else						/*check whether the node was left child of its parent*/
+				else								/*check whether the node was left child of its parent*/
 					parent->left = NULL;
 				break;
 			}
-			if(currNode->left == NULL || currNode->right == NULL)		/*check for node having only one child*/
+			if(currNode->left == NULL || currNode->right == NULL)			/*check for node having only one child*/
 			{
-				if(currNode->left == NULL)				/*check for node having only right subtree*/
+				if(currNode->left == NULL)					/*check for node having only right subtree*/
 				{
-					if(parent == NULL)								/*check if there is only root with one right child in the tree*/
+					if(parent == NULL)					/*check if there is only root with one right child in the tree*/
 					{
 						*proot = currNode->right;
 						break;
 					}
-					if(parent->right == currNode)			/*check whether the current node was right child of its parent*/
+					if(parent->right == currNode)				/*check whether the current node was right child of its parent*/
 						parent->right = currNode->right;
-					else					/*check whether the current node was left child of its parent*/
+					else							/*check whether the current node was left child of its parent*/
 						parent->left = currNode->right;
 				}
-				else						/*check for node having only left subtree*/
+				else								/*check for node having only left subtree*/
 				{
-					if(parent == NULL)								/*check if there is only root with one left child in the tree*/
+					if(parent == NULL)					/*check if there is only root with one left child in the tree*/
 					{
 						*proot = currNode->left;
 						break;
 					}
-					if(parent->right == currNode)			/*check whether the current node was right child of its parent*/
+					if(parent->right == currNode)				/*check whether the current node was right child of its parent*/
 						parent->right = currNode->left;
-					else					/*check whether the current node was left child of its parent*/
+					else							/*check whether the current node was left child of its parent*/
 						parent->left = currNode->left;
 				}
 				break;
 			}
 			else
 			{
-				insucc = currNode->left;				/*holds the left child of the node to be deleted*/
-				while(insucc->right != NULL)				/*finds the inorder successor of the node to be deleted*/
+				insucc = currNode->left;					/*holds the left child of the node to be deleted*/
+				while(insucc->right != NULL)					/*finds the inorder successor of the node to be deleted*/
 				{
-					pinsucc = insucc;				/*holds the parent of successor node*/
+					pinsucc = insucc;					/*holds the parent of successor node*/
 					insucc = insucc->right;
 				}
-				if(parent != NULL)					/*check if the node to be deleted is root node*/
+				if(parent != NULL)						/*check if the node to be deleted is root node*/
 				{
 					if(parent->right == currNode)
 						parent->right = insucc;
@@ -206,25 +206,25 @@ void deleteNode(NODEPTR *proot, int val)							/*function definition to delete a
 				else
 					*proot = insucc;
 				insucc->right = currNode->right;				/*right subtree of deleted node becomes right subtree of successor node*/
-				if(currNode->left != insucc)				/*check whether the left child of deleted node is not the successor*/
+				if(currNode->left != insucc)					/*check whether the left child of deleted node is not the successor*/
 				{	
-					temp = insucc;							/*Holds the successor node*/
+					temp = insucc;						/*Holds the successor node*/
 					while(temp->left != NULL)				/*finds the leftmost child of successor node*/
 						temp = temp->left;
-					temp->left = currNode->left;			/*the leftmost child of successor node will hold the left subtree of deleted node*/
+					temp->left = currNode->left;				/*the leftmost child of successor node will hold the left subtree of deleted node*/
 				}
-				if(currNode->left == insucc)				/*check whether the left child of deleted node is the only successor*/
+				if(currNode->left == insucc)					/*check whether the left child of deleted node is the only successor*/
 					insucc->left == currNode->left->left;  
-				if(pinsucc != NULL)					/*check if there is a parent of successor node*/
-					pinsucc->right = NULL;			/*parent node of successor has now no child*/
+				if(pinsucc != NULL)						/*check if there is a parent of successor node*/
+					pinsucc->right = NULL;					/*parent node of successor has now no child*/
 				break;
 			}
 		}
-		parent = currNode;							/*update parent node*/
-		if(currNode->info < val)						/*check if the value to be deleted is greater than current node value*/
+		parent = currNode;								/*update parent node*/
+		if(currNode->info < val)							/*check if the value to be deleted is greater than current node value*/
 			currNode = currNode->right;
 			
-		if(currNode->info > val)						/*check if the value to be deleted is less than current node value*/
+		if(currNode->info > val)							/*check if the value to be deleted is less than current node value*/
 			currNode = currNode->left;
 	}while(flag != 1);
 	
@@ -232,40 +232,40 @@ void deleteNode(NODEPTR *proot, int val)							/*function definition to delete a
 	
 	free(delNode);
 }
-void preorder(NODEPTR proot)								/*function definition for PREORDER traversal*/
+void preorder(NODEPTR proot)									/*function definition for PREORDER traversal*/
 {
 	if(proot == NULL)
 		return;
-	printf(" %d", proot->info);							/*Print the node value*/
-	preorder(proot->left);								/*recursive call to left child*/
-	preorder(proot->right);							/*recursive call to right child*/
+	printf(" %d", proot->info);								/*Print the node value*/
+	preorder(proot->left);									/*recursive call to left child*/
+	preorder(proot->right);									/*recursive call to right child*/
 }
-void postorder(NODEPTR proot)							/*function definition for POSTORDER traversal*/
+void postorder(NODEPTR proot)									/*function definition for POSTORDER traversal*/
 {
 	if(proot == NULL)
 		return;
-	postorder(proot->left);							/*recursive call to left child*/
-	postorder(proot->right);							/*recursive call to right child*/
-	printf(" %d", proot->info);							/*print the node value*/
+	postorder(proot->left);									/*recursive call to left child*/
+	postorder(proot->right);								/*recursive call to right child*/
+	printf(" %d", proot->info);								/*print the node value*/
 }
-void inorder(NODEPTR proot)								/*function definition for INORDER traversal*/
+void inorder(NODEPTR proot)									/*function definition for INORDER traversal*/
 {
 	if(proot == NULL)
 		return;
-	inorder(proot->left);								/*recursive call to left child*/
-	printf(" %d", proot->info);							/*print the node value*/
-	inorder(proot->right);							/*recursive call to right child*/
+	inorder(proot->left);									/*recursive call to left child*/
+	printf(" %d", proot->info);								/*print the node value*/
+	inorder(proot->right);									/*recursive call to right child*/
 }
-int countNode(NODEPTR proot)							/*function definition to count total nodes present in BST*/
+int countNode(NODEPTR proot)									/*function definition to count total nodes present in BST*/
 {
-	if(proot == NULL)								/*check for empty BST*/
+	if(proot == NULL)									/*check for empty BST*/
 		return 0;
 	else
 		return 1 + countNode(proot->left) + countNode(proot->right);
 }
-int treeHeight(NODEPTR proot)								/*function definition to calculate height of BST*/
+int treeHeight(NODEPTR proot)									/*function definition to calculate height of BST*/
 {
-	if(proot == NULL)								/*check for empty bst*/
+	if(proot == NULL)									/*check for empty bst*/
 		return 0;
 	else
 		return max(treeHeight(proot->left),treeHeight(proot->right)) + 1;
@@ -277,18 +277,18 @@ int max(int a, int b)
 	else
 		return b;
 }
-int search(NODEPTR proot, int key)							/*function definition to search for a particular node*/
+int search(NODEPTR proot, int key)								/*function definition to search for a particular node*/
 {
-	if(proot == NULL)								/*check for empty BST*/
+	if(proot == NULL)									/*check for empty BST*/
 		return 0;
 	while(proot != NULL)
 	{
-		if(key < proot->info)							/*check whether value is less than current node's value*/
+		if(key < proot->info)								/*check whether value is less than current node's value*/
 			proot = proot->left;
-		else if(key > proot->info)						/*check whether value is more than current node's value*/
+		else if(key > proot->info)							/*check whether value is more than current node's value*/
 			proot = proot->right;
 		else			
-			return 1;							/*value found*/
+			return 1;								/*value found*/
 	}
-	return 0;									/*unsuccessful search.*/
+	return 0;										/*unsuccessful search.*/
 }
